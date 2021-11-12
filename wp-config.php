4492d6f3ee0503d05899f9b02704f84a4631e49d
Raja Salomo Sitompul <v.raja.sitompul@adira.co.id>
2021-11-12 13:06:23 +0700

    DDD

--- wp-config.php
+++ wp-config.php
@@ -32,7 +32,7 @@ define( 'DB_PASSWORD', 'd9lrwhxqe59j21fv' );
 define( 'DB_HOST', 'dcrhg4kh56j13bnu.cbetxkdyhwsb.us-east-1.rds.amazonaws.com' );
 
 /** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8mb4' );
+define( 'DB_CHARSET', 'utf8' );
 
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
@@ -48,14 +48,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         '}<tEyAAF r}f:~+Aa7Ia4UFnkC(kDXndeS(/xh;f+lP,U|{,,Be$olyZi(F(Fu+c' );
-define( 'SECURE_AUTH_KEY',  ',q>% JtDLB}.b=WoM[`wYk!=Z9CD*i1+Gw `7;^,DYZKZ~OQ7G1r+luR/ompM uO' );
-define( 'LOGGED_IN_KEY',    'HE:j;]XDW|% mO=|7yTkm~H3.3u^e- i[vax;_|=~8go@[TOQ&oR8GUDU^&RpQI+' );
-define( 'NONCE_KEY',        'H@jFJr}U$!^rd{M8r@/,-|eQH-8TM<*)rO<5)*gq`.!*B-+mmxS,l~plzt[ATB8L' );
-define( 'AUTH_SALT',        ']SMGRHAG~XT/JXUc,5O{|DRjDm&r}ztQ48kLLt{RE_]6w;q)OYn_.V)HC?-g3~DC' );
-define( 'SECURE_AUTH_SALT', 'ow*x0F,*{TXzrj,t`V75P_x*&geN1~N#C*.zs~E[3C6;3y;52XJLY70>=a(YF]sf' );
-define( 'LOGGED_IN_SALT',   'b}me!z@<Qo).zz->oBy[ q<$|Ztav.[>4a8D5LrwjqVfJRNZ^)54rEx1J;=$z2zn' );
-define( 'NONCE_SALT',       'W2dN^[|dC}r<ZC1wD@=KBC/jx%O97K0 l,Z#<=PTzuX+SK`G_pm-;gCrb2,Uh;sv' );
+define( 'AUTH_KEY',         'put your unique phrase here' );
+define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
+define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
+define( 'NONCE_KEY',        'put your unique phrase here' );
+define( 'AUTH_SALT',        'put your unique phrase here' );
+define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
+define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
+define( 'NONCE_SALT',       'put your unique phrase here' );
 
 /**#@-*/
 
